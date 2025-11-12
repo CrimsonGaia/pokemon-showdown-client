2beed71cbdce70b4870a871dfe02bc740c0a3c09
CrimsonGaia <CrimsonGaiaofficial@gmail.com>
2025-11-12 07:40:50 -0800

    test

--- config/config-example.js
+++ config/config-example.js
@@ -20,7 +20,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'sim3.psim.us',
+	host: 'localhost',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
@@ -31,5 +31,6 @@ Config.roomsFirstOpenScript = function () {
 };
 
 Config.customcolors = {
-	'zarel': 'aeo'
+	'zarel': 'aeo',
+	'crimson gaia' : 'aeo'
 };
