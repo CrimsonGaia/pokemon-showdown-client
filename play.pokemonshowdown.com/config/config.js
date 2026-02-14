d8a8639f9190554c39cb2efbfb31ac5b7d42369d
CrimsonGaia <CrimsonGaiaofficial@gmail.com>
2026-02-13 21:36:28 -0800

    server client connection
    Client now automatically copies to the servercache at end of build process. Server now serves client files. Works on LAN only for now

--- play.pokemonshowdown.com/config/config.js
+++ play.pokemonshowdown.com/config/config.js
@@ -7,42 +7,45 @@ Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
 Config.whitelist = [
 	'wikipedia.org',
-
-	// The full list is maintained outside of this repository so changes to it
-	// don't clutter the commit log. Feel free to copy our list for your own
-	// purposes; it's here: https://play.pokemonshowdown.com/config/config.js
-
-	// If you would like to change our list, simply message Zarel on Smogon or
-	// Discord.
 ];
 
-// `defaultserver` specifies the server to use when the domain name in the
-// address bar is `Config.routes.client`.
+// Single-server, same-origin setup.
+// This makes the client use whatever host/port it was loaded from.
+const ORIGIN_HOST = location.hostname;
+const ORIGIN_PORT = location.port ? Number(location.port) : (location.protocol === 'https:' ? 443 : 80);
+
 Config.defaultserver = {
-	id: 'showdown',
-	host: 'localhost',
-	port: 8000,
-	httpport: 8000,
-	altport: 8000,
-	registered: true
+	id: 'server',
+	host: ORIGIN_HOST,
+	port: ORIGIN_PORT,
+	httpport: ORIGIN_PORT,
+	altport: ORIGIN_PORT,
+	registered: true,
 };
 
-Config.roomsFirstOpenScript = function () {
-};
+Config.servers = Config.servers || {};
+Config.servers.server = Config.defaultserver;
+
+// Optional: make the "auto" chooser always pick this
+Config.servers.public = Config.defaultserver;
+
+Config.roomsFirstOpenScript = function () {};
 
 Config.customcolors = {
 	'zarel': 'aeo',
-	'crimson gaia' : 'aeo'
+	'crimson gaia': 'aeo',
 };
+
 /*** Begin automatically generated configuration ***/
 Config.version = "0.11.2 (2f9254d2/adb6b7fc)";
 
+// Make the client treat your current host as the "client" route,
+// so any host-based routing logic lines up.
 Config.routes = {
-	root: 'pokemonshowdown.com',
-	client: 'play.pokemonshowdown.com',
+	root: location.host,
+	client: location.host,
 	dex: 'dex.pokemonshowdown.com',
 	replays: 'replay.pokemonshowdown.com',
-	users: 'pokemonshowdown.com/users',
-	teams: 'teams.pokemonshowdown.com',
+	users: 'insecure.psim.us/users',
+	teams: 'insecure.psim.us',
 };
-/*** End automatically generated configuration ***/
